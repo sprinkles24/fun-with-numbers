bdd65b8cb4afc685d5380492d0e20108e579dfdb
sprinkles24 <shylasingal@gmail.com>
2025-05-29 15:56:34 -0400

    fixed it so it works, also working with theorchidpenguin

--- script.py
+++ script.py
@@ -1,20 +1,32 @@
 
-def happyCalculations(givenNumStr):
+def happyCalculations(givenNumInt):
+    givenNumInt = str(givenNumInt)
     currentSum = 0
-    for digit in givenNumStr:
+    for digit in givenNumInt:
         digit = int(digit)
         currentSum += (digit ** 2)
+    return currentSum
 
 
 def happyNumbers(originalNumStr):
-    tempNum = None
-    while (tempNum != 1) and (tempNum != originalNumStr): #repeat until (tempNum == 1) or (tempNum == originalMum)
-        tempNum = happyCalculations(originalNumStr)
+    usedNumsList = []
+    tempNum = int(originalNumStr)
+    while tempNum != 1 and tempNum not in usedNumsList:
+        usedNumsList.append(tempNum)
+        tempNum = happyCalculations(str(tempNum))
     if tempNum == 1:
-        return ("Happy")
-    elif tempNum == originalNumStr:
-        return ("Not happy")
+        return "Happy"
+    elif tempNum in usedNumsList:
+        return "Not happy"
+    else:
+        return "there's an error lol"
+    
+
+
 
 print("Hello world!!!")
     
 print(happyNumbers("13"))
+
+print("hi")
+
