5abc9b76ef2c5854368a45231034f702db9fab3f
sprinkles24 <shylasingal@gmail.com>
2025-05-29 12:30:28 -0400

    finished happyCalculations procedure and happyNumbers procedure

--- script.py
+++ script.py
@@ -1,2 +1,19 @@
-print("Hello World")
-#hi!
+
+def happyCalculations(givenNumStr):
+    currentSum = 0
+    for digit in givenNumStr:
+        digit = int(digit)
+        currentSum += (digit ** 2)
+
+
+def happyNumbers(originalNumStr):
+    tempNum = None
+    while (tempNum != 1) and (tempNum != originalNumStr): #repeat until (tempNum == 1) or (tempNum == originalMum)
+        tempNum = happyCalculations(originalNumStr)
+    if tempNum == 1:
+        return ("Happy")
+    elif tempNum == originalNumStr:
+        return ("Not happy")
+
+
+    
